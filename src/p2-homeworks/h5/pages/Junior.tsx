dc315f77ccfae3d88febe188af7d247c62b7ca8c
serIvanich <serivan1108@gmail.com>
2021-06-01 15:12:03 +0300

    hw9 min

--- src/p2-homeworks/h5/pages/Junior.tsx
+++ src/p2-homeworks/h5/pages/Junior.tsx
@@ -2,12 +2,14 @@ import React from "react"
 
 import {HW7} from "../../h7/HW7"
 import HW8 from "../../h8/HW8"
+import HW9 from "../../h9/HW9";
 
 function Junior() {
     return (
         <div style={{margin: '5px'}}>
             <HW7/>
             <HW8/>
+            <HW9/>
         </div>
     )
 
--- src/p2-homeworks/h9/Clock.tsx
+++ src/p2-homeworks/h9/Clock.tsx
@@ -3,47 +3,59 @@ import SuperButton from '../h4/common/c2-SuperButton/SuperButton'
 
 function Clock() {
     const [timerId, setTimerId] = useState<number>(0)
-    const [date, setDate] = useState<Date>()
+    const [date, setDate] = useState<Date>(new Date())
     const [show, setShow] = useState<boolean>(false)
 
     const stop = () => {
-        // stop
+        clearInterval(timerId)
     }
     const start = () => {
         stop()
         const id: number = window.setInterval(() => {
-            // setDate
+            setDate(new Date())// setDate
         }, 1000)
         setTimerId(id)
     }
 
     const onMouseEnter = () => {
-        // show
+        setShow(true)
     }
     const onMouseLeave = () => {
-        // close
+        setShow(false)// close
     }
-
-    const stringTime = 'Time' // fix with date
-    const stringDate = 'Date' // fix with date
+    console.log(date)
+    const stringTime = date.toTimeString().slice(0,8) // fix with date
+    const stringDate =`${date.getFullYear()} 0${date.getMonth() + 1} 0${date.getDate()}`// fix with date
 
     return (
         <div>
             <div
                 onMouseEnter={onMouseEnter}
                 onMouseLeave={onMouseLeave}
+                style={{
+                    height: '40px',
+                    width: '80px',
+                    margin: '5px',
+                    padding: '5px',
+                    border: '1px solid',
+                    borderRadius: '5px',
+                    backgroundColor: 'darkorange',
+                    color: 'whitesmoke',
+                }}
             >
                 {stringTime}
-            </div>
+
 
             {show && (
                 <div>
+
                     {stringDate}
                 </div>
             )}
-
+            </div>
             <SuperButton onClick={start}>start</SuperButton>
-            <SuperButton onClick={stop}>stop</SuperButton>
+            <SuperButton style={{marginLeft: '5px'}}
+                         onClick={stop}>stop</SuperButton>
 
         </div>
     )
